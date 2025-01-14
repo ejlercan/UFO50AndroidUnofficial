b82c8bce043621a3da94d195cb20da7933db6555
Logan Baron <logan.baron@gmail.com>
2025-01-14 11:28:24 -0600

    Simplified save transfers to not need the user to specify a device ID or mess around with folders.

--- upload_saves.bat
+++ upload_saves.bat
@@ -1,13 +1,13 @@
 @echo off
 
-set devid=DEVICEIDHERE
-
-.\bin\adb.exe -s %devid% shell "run-as com.unofficial.ufo50 mkdir /sdcard/UFO50Backup"
-.\bin\adb.exe -s %devid% push .\save\upload\save1.ufo /sdcard/UFO50Backup/
-.\bin\adb.exe -s %devid% push .\save\upload\save2.ufo /sdcard/UFO50Backup/
-.\bin\adb.exe -s %devid% push .\save\upload\save3.ufo /sdcard/UFO50Backup/
-.\bin\adb.exe -s %devid% push .\save\upload\account.ufo /sdcard/UFO50Backup/
-.\bin\adb.exe -s %devid% shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/save1.ufo /data/data/com.unofficial.ufo50/files/"
-.\bin\adb.exe -s %devid% shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/save2.ufo /data/data/com.unofficial.ufo50/files/"
-.\bin\adb.exe -s %devid% shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/save3.ufo /data/data/com.unofficial.ufo50/files/"
-.\bin\adb.exe -s %devid% shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/account.ufo /data/data/com.unofficial.ufo50/files/"
+for /f %%i in ('.\bin\adb devices^|findstr /e "device"') do (
+	.\bin\adb.exe -s %%i shell "run-as com.unofficial.ufo50 mkdir /sdcard/UFO50Backup"
+	.\bin\adb.exe -s %%i push .\save\save1.ufo /sdcard/UFO50Backup/
+	.\bin\adb.exe -s %%i push .\save\save2.ufo /sdcard/UFO50Backup/
+	.\bin\adb.exe -s %%i push .\save\save3.ufo /sdcard/UFO50Backup/
+	.\bin\adb.exe -s %%i push .\save\account.ufo /sdcard/UFO50Backup/
+	.\bin\adb.exe -s %%i shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/save1.ufo /data/data/com.unofficial.ufo50/files/"
+	.\bin\adb.exe -s %%i shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/save2.ufo /data/data/com.unofficial.ufo50/files/"
+	.\bin\adb.exe -s %%i shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/save3.ufo /data/data/com.unofficial.ufo50/files/"
+	.\bin\adb.exe -s %%i shell "run-as com.unofficial.ufo50 cp -f /sdcard/UFO50Backup/account.ufo /data/data/com.unofficial.ufo50/files/"
+)
